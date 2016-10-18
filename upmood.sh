b427807781abbffaa8298a3fcfd1495597b5acfc
marcedev <it.marcellin.college@gmail.com>
2016-10-19 10:02:11 +1100

    marcellin entries - 1

--- upmood.sh
+++ upmood.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
    git fetch upstream
    for BRANCH in  master; do
-       git push origin refs/remotes/upstream/$BRANCH:$BRANCH
+       git push origin refs/remotes/upstream/$BRANCH:$BRANCH 
    done
